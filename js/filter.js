02ee06115a289f5d767b482294b76cba653141e3
anastasiia <kholina.a@gmail.com>
2021-11-07 20:36:19 +0100

    Правки

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,10 @@
 import {drawAdverts, getOriginalAdverts, setFilteredAdverts, SIMILAR_ADVERTS_COUNT} from './map.js';
 import {debounce} from './utils/debounce.js';
 
+const LOW_PRICE = 10000;
+const HIGH_PRICE = 50000;
+const ROOMS_NOT_FOR_GUESTS = 100;
+
 const mapFilters = document.querySelector('.map__filters');
 const housingTypeFilter = mapFilters.querySelector('#housing-type');
 const priceFilter = mapFilters.querySelector('#housing-price');
@@ -9,85 +13,88 @@ const guestsFilter = mapFilters.querySelector('#housing-guests');
 const featuresFilter = mapFilters.querySelector('#housing-features');
 const features = featuresFilter.querySelectorAll('input[type=checkbox]');
 
-const LOW_PRICE = 10000;
-const HIGH_PRICE = 50000;
-const ROOMS_NOT_FOR_GUESTS = 100;
-
 let selectedHousingType;
 let selectedPrice;
 let selectedRooms;
 let selectedGuests;
 let selectedFeatures = [];
 
-const filterSimilarAdverts = () => {
-  const filteredAdverts = getOriginalAdverts()
-    .filter((advert) => {
-      if (selectedHousingType && selectedHousingType !== 'any') {
-        return advert.offer.type === selectedHousingType;
-      }
-      return true;
-    })
-    .filter((advert) => {
-      if (selectedPrice && selectedPrice !== 'any') {
-        if (selectedPrice === 'low') {
-          return advert.offer.price < LOW_PRICE;
-        } else if (selectedPrice === 'middle') {
-          return advert.offer.price >= LOW_PRICE && advert.offer.price <= HIGH_PRICE;
-        } else if (selectedPrice === 'high') {
-          return advert.offer.price > HIGH_PRICE;
-        }
-      } else {
-        return true;
-      }
-    })
-    .filter((advert) => {
-      if (selectedRooms && selectedRooms !== 'any') {
-        return advert.offer.rooms === Number(selectedRooms);
-      }
-      return true;
-    })
-    .filter((advert) => {
-      if (selectedGuests && selectedGuests !== 'any' && selectedGuests !== '0') {
-        return advert.offer.guests >= Number(selectedGuests);
-      } else if (selectedGuests === '0') {
-        return advert.offer.rooms === ROOMS_NOT_FOR_GUESTS;
-      } else {
-        return true;
-      }
-    })
-    .filter ((advert) => {
-      if (selectedFeatures && selectedFeatures.length > 0) {
-        if (advert.offer.features) {
-          return selectedFeatures.every((feature) => advert.offer.features.includes(feature));
-        }
-        return false;
-      }
-      return true;
-    })
-    .slice(0, SIMILAR_ADVERTS_COUNT);
-  setFilteredAdverts(filteredAdverts);
-  const debouncedDrawAdvert = debounce(drawAdverts);
-  debouncedDrawAdvert();
+const filterHousingType = (advert) => selectedHousingType && selectedHousingType !== 'any' ? advert.offer.type === selectedHousingType : true;
+
+const filterPrice = (advert) => {
+  if (selectedPrice && selectedPrice !== 'any') {
+    if (selectedPrice === 'low') {
+      return advert.offer.price < LOW_PRICE;
+    }
+
+    if (selectedPrice === 'middle') {
+      return advert.offer.price >= LOW_PRICE && advert.offer.price <= HIGH_PRICE;
+    }
+
+    if (selectedPrice === 'high') {
+      return advert.offer.price > HIGH_PRICE;
+    }
+  }
+
+  return true;
 };
+
+const filterRooms = (advert) => selectedRooms && selectedRooms !== 'any' ? advert.offer.rooms === Number(selectedRooms) : true;
+
+const filterGuests = (advert) => {
+  if (selectedGuests && selectedGuests !== 'any' && selectedGuests !== '0') {
+    return advert.offer.guests >= Number(selectedGuests);
+  }
+
+  if (selectedGuests === '0') {
+    return advert.offer.rooms === ROOMS_NOT_FOR_GUESTS;
+  }
+
+  return true;
+};
+
+const filterFeatures = (advert) => {
+  if (selectedFeatures && selectedFeatures.length > 0) {
+    return advert.offer.features ? selectedFeatures.every((feature) => advert.offer.features.includes(feature)) : false;
+  }
+  return true;
+};
+
+const filterAdverts = (advert) => filterHousingType(advert) && filterPrice(advert) && filterRooms(advert) && filterGuests(advert) && filterFeatures(advert);
+
+const showFilteredAdverts = function() {
+  const filteredAdverts = getOriginalAdverts()
+    .filter(function(advert) {
+      if (this.count < SIMILAR_ADVERTS_COUNT && filterAdverts(advert)) {
+        this.count++;
+        return true;
+      }
+      return false;
+    }, {count: 0});
+  setFilteredAdverts(filteredAdverts);
+  drawAdverts();
+};
+
+const debouncedFilteredAdverts = debounce(showFilteredAdverts);
 
 housingTypeFilter.addEventListener('change', () => {
   selectedHousingType = housingTypeFilter.value;
-  filterSimilarAdverts();
+  debouncedFilteredAdverts();
 });
 
 priceFilter.addEventListener('change', () => {
   selectedPrice = priceFilter.value;
-  filterSimilarAdverts();
+  debouncedFilteredAdverts();
 });
 
 roomsFilter.addEventListener('change', () => {
   selectedRooms = roomsFilter.value;
-  filterSimilarAdverts();
+  debouncedFilteredAdverts();
 });
 
 guestsFilter.addEventListener('change', () => {
   selectedGuests = guestsFilter.value;
-  filterSimilarAdverts();
+  debouncedFilteredAdverts();
 });
 
 featuresFilter.addEventListener('change', () => {
@@ -98,6 +105,6 @@ featuresFilter.addEventListener('change', () => {
     }
   });
   selectedFeatures = selectedFeaturesArray;
-  filterSimilarAdverts();
+  debouncedFilteredAdverts();
 });
 
